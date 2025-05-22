759d6bce6b89514d745cfc233ea3c32bf6623951
Ivaylo Bachvarov <ISBachvarov21@codingburgas.bg>
2025-05-22 23:19:32 +0300

    Implement FileWatcher class

--- include/FileWatcher.h
+++ include/FileWatcher.h
@@ -1,10 +1,25 @@
 #ifndef FILEWATCHER_H
 #define FILEWATCHER_H
+#include <atomic>
+#include <stop_token>
+#include <string>
+#include <filesystem>
+
 #include "Event.h"
 
 namespace Utils {
 
 class FileWatcher {
+public:
+    explicit FileWatcher(std::string  dir);
+    ~FileWatcher() = default;
+
+    void watch(const std::stop_token &stoken, std::atomic<bool>& needsReload);
+
+private:
+    std::string m_dir;
+    std::filesystem::file_time_type m_lastWriteTime;
+
 public:
     Event<void> onFileChanged;
 };
--- src/FileWatcher.cpp
+++ src/FileWatcher.cpp
@@ -5,4 +5,23 @@
 #include "../include/FileWatcher.h"
 
 namespace Utils {
+
+FileWatcher::FileWatcher(std::string  dir) : m_dir(std::move(dir)) {}
+
+void FileWatcher::watch(const std::stop_token &stoken, std::atomic<bool> &needsReload) {
+    while (!stoken.stop_requested()) {
+        for (const auto &entry : std::filesystem::directory_iterator(m_dir)) {
+            if (entry.is_regular_file()) {
+                auto lastWriteTime = std::filesystem::last_write_time(entry.path());
+                if (lastWriteTime > m_lastWriteTime) {
+                    m_lastWriteTime = lastWriteTime;
+                    onFileChanged.Invoke();
+                    needsReload.store(true);
+                }
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
+
 } // Utils
